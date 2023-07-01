d110cca4ef02792a275ed5147722669b57d9f5f0
ramandabhijana <abhijanaramanda@gmail.com>
2023-07-01 11:37:45 +0800

    make the test green & refactor

--- src/Applications/use_case/get_thread_detail/GetThreadDetailUseCase.ts
+++ src/Applications/use_case/get_thread_detail/GetThreadDetailUseCase.ts
@@ -1,7 +1,7 @@
 import type CommentRepositoryBase from '../../../Domains/comments/CommentRepositoryBase'
 import type ReplyRepositoryBase from '../../../Domains/replies/ReplyRepositoryBase'
 import type ThreadRepositoryBase from '../../../Domains/threads/ThreadRepositoryBase'
-import DetailedThread, { type Comments, MAX_COMMENTS_COUNT, MAX_REPLIES_PER_COMMENT_COUNT, type DetailedThreadPayload } from '../../../Domains/threads/entities/DetailedThread'
+import DetailedThread, { MAX_COMMENTS_COUNT, MAX_REPLIES_PER_COMMENT_COUNT, type DetailedThreadPayload } from '../../../Domains/threads/entities/DetailedThread'
 import { UseCaseBase } from '../UseCaseBase'
 
 class GetThreadDetailUseCase extends UseCaseBase<string, DetailedThreadPayload> {
@@ -17,20 +17,8 @@ class GetThreadDetailUseCase extends UseCaseBase<string, DetailedThreadPayload> 
     await this.threadRepository.verifyThreadExists(payload)
 
     const threadPromise = this.threadRepository.getThreadWithUsernameById(payload)
-    const commentsPromise = this.commentRepository.getCommentsWithUsernameByThreadId(payload, { limit: MAX_COMMENTS_COUNT })
-    const [thread, commentsWithUsername] = await Promise.all([threadPromise, commentsPromise])
-
-    const comments: Comments = commentsWithUsername.map((comment) => ({
-      ...comment,
-      replies: []
-    }))
-
-    await Promise.all(
-      comments.map(async (comment, index) => {
-        const replies = await this.replyRepository.getRepliesWithUsernameByCommentId(comment.id, { limit: MAX_REPLIES_PER_COMMENT_COUNT })
-        comments[index].replies = replies
-      })
-    )
+    const commentsPromise = this.commentRepository.getCommentsByThreadId(payload, { limit: MAX_COMMENTS_COUNT }, { limit: MAX_REPLIES_PER_COMMENT_COUNT })
+    const [thread, comments] = await Promise.all([threadPromise, commentsPromise])
 
     return new DetailedThread({ ...thread, comments }).asObject
   }
