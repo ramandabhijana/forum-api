c003214e500ea450ba8df2cf06db23947fac78ab
ramandabhijana <abhijanaramanda@gmail.com>
2023-07-15 21:29:01 +0800

    add getThreadCommentsById abstract method

--- src/Domains/threads/ThreadRepositoryBase.ts
+++ src/Domains/threads/ThreadRepositoryBase.ts
@@ -1,10 +1,13 @@
 import { type AddedThreadPayload } from './entities/AddedThread'
 import { type ThreadWithUsernamePayload } from './entities/ThreadWithUsername'
+import { type DetailedThreadPayload } from './entities/DetailedThread'
+import { type PaginationOptions } from '../../Commons/types/Types'
 
 abstract class ThreadRepositoryBase {
   abstract addThread(title: string, body: string, owner: string): Promise<AddedThreadPayload>
   abstract verifyThreadExists(threadId: string): Promise<void>
   abstract getThreadWithUsernameById(threadId: string): Promise<ThreadWithUsernamePayload>
+  abstract getThreadCommentsById(threadId: string, commentsQueryOptions?: Partial<PaginationOptions>): Promise<DetailedThreadPayload>
 }
 
 export default ThreadRepositoryBase
