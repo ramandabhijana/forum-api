09821baa6eb92ce8af1fe03a66becbf05a2f3f1d
ramandabhijana <abhijanaramanda@gmail.com>
2023-07-16 17:49:53 +0800

    remove unused functions

--- src/Domains/comments/CommentRepositoryBase.ts
+++ src/Domains/comments/CommentRepositoryBase.ts
@@ -1,14 +1,11 @@
 import { type PaginationOptions } from '../../Commons/types/Types'
-import { type CommentWithReplies, type Comments } from '../threads/entities/DetailedThread'
+import { type CommentWithReplies } from '../threads/entities/DetailedThread'
 import { type AddedCommentPayload } from './entities/AddedComment'
-import { type CommentWithUsernamePayload } from './entities/CommentWithUsername'
 
 abstract class CommentRepositoryBase {
   abstract addComment(content: string, threadId: string, owner: string): Promise<AddedCommentPayload>
   abstract verifyCommentExists(commentId: string): Promise<void>
   abstract verifyCommentOwner(commentId: string, owner: string): Promise<void>
-  abstract getCommentsWithUsernameByThreadId(threadId: string, options?: Partial<PaginationOptions>): Promise<CommentWithUsernamePayload[]>
-  abstract getCommentsByThreadId(threadId: string, commentsQueryOptions?: Partial<PaginationOptions>, repliesQueryOptions?: Partial<PaginationOptions>): Promise<Comments>
   abstract getCommentRepliesByCommentIds(commentIds: string[], repliesQueryOptions?: Partial<PaginationOptions>): Promise<Map<string, CommentWithReplies>>
   abstract deleteComment(commentId: string): Promise<void>
 }
--- src/Infrastructures/repository/comments/CommentRepository.ts
+++ src/Infrastructures/repository/comments/CommentRepository.ts
@@ -7,8 +7,7 @@ import NotFoundError from '../../../Commons/exceptions/NotFoundError'
 import { COMMENT_NOT_FOUND, COMMENT_OWNER_NOT_AUTHORIZED } from '../../../Commons/exceptions/messages/ErrorMessages'
 import AuthorizationError from '../../../Commons/exceptions/AuthorizationError'
 import { type PaginationOptions } from '../../../Commons/types/Types'
-import CommentWithUsername, { type CommentWithUsernamePayload } from '../../../Domains/comments/entities/CommentWithUsername'
-import { type CommentWithReplies, type Comments } from '../../../Domains/threads/entities/DetailedThread'
+import { type CommentWithReplies } from '../../../Domains/threads/entities/DetailedThread'
 import { Reply } from '../replies/model/Reply'
 import ReplyWithUsername from '../../../Domains/replies/entities/ReplyWithUsername'
 
@@ -59,102 +58,6 @@ class CommentRepository extends CommentRepositoryBase {
     if (comment.commenter.id !== owner) {
       throw new AuthorizationError(COMMENT_OWNER_NOT_AUTHORIZED)
     }
-  }
-
-  async getCommentsByThreadId(threadId: string, commentsQueryOptions?: Partial<PaginationOptions>, repliesQueryOptions?: Partial<PaginationOptions>): Promise<Comments> {
-    const repliesLimit = repliesQueryOptions?.limit ?? 10
-    const repliesOffset = repliesQueryOptions?.offset ?? 0
-    const subQuery = this.replyRepository
-      .createQueryBuilder('reply')
-      .withDeleted()
-      .select('reply.id')
-      .where('reply.comment = comment.id')
-      .skip(repliesOffset)
-      .take(repliesLimit)
-      .getQuery()
-
-    const commentsLimit = commentsQueryOptions?.limit ?? 10
-    const commentsOffset = commentsQueryOptions?.offset ?? 0
-    const comments = await this.repository.createQueryBuilder('comment')
-      .withDeleted()
-      .leftJoinAndSelect('comment.commenter', 'commenter')
-      .leftJoinAndSelect('comment.replies', 'reply', `reply.id IN (${subQuery})`)
-      .leftJoinAndSelect('reply.replier', 'replier')
-      .select([
-        'comment.id',
-        'comment.content',
-        'comment.createdAt',
-        'comment.deletedAt',
-        'commenter.username',
-        'reply.id',
-        'reply.content',
-        'reply.createdAt',
-        'reply.deletedAt',
-        'replier.username'
-      ])
-      .where('comment.thread_id = :threadId', { threadId })
-      .orderBy('comment.createdAt', 'ASC')
-      .skip(commentsOffset)
-      .take(commentsLimit)
-      .getMany()
-
-    return comments.map(commentEntity => {
-      const comment = new CommentWithUsername({
-        id: commentEntity.id,
-        content: commentEntity.content,
-        createdAt: commentEntity.createdAt,
-        deletedAt: commentEntity.deletedAt,
-        username: commentEntity.commenter.username
-      }).asObject
-
-      // Strange behavior, the `take` expr applied to comments is not functioning properly when `addOrderBy` is used to sort replies
-      // Try add `.addOrderBy('reply.createdAt', 'ASC')` after `orderBy` was called
-      // Decided to sort replies using standard js sort array method.
-      // This implies that `getCommentsByThreadId` shall not be provided with large number to limit the replies returned
-      const replies = commentEntity.replies
-        .map(replyEntity => new ReplyWithUsername({
-          id: replyEntity.id,
-          content: replyEntity.content,
-          createdAt: replyEntity.createdAt,
-          deletedAt: replyEntity.deletedAt,
-          username: replyEntity.replier.username
-        }).asObject)
-        .sort((lhs, rhs) => lhs.date.getTime() - rhs.date.getTime())
-
-      return { ...comment, replies }
-    })
-  }
-
-  async getCommentsWithUsernameByThreadId(threadId: string, options?: Partial<PaginationOptions>): Promise<CommentWithUsernamePayload[]> {
-    const limit = options?.limit ?? 10
-    const offset = options?.offset ?? 0
-
-    const comments = await this.repository
-      .createQueryBuilder('comment')
-      .leftJoin('comment.commenter', 'commenter')
-      .select([
-        'comment.id',
-        'comment.content',
-        'comment.createdAt',
-        'comment.deletedAt',
-        'commenter.username'
-      ])
-      .withDeleted()
-      .where('comment.thread_id = :threadId', { threadId })
-      .orderBy('comment.createdAt', 'ASC')
-      .skip(offset)
-      .take(limit)
-      .getMany()
-
-    return comments.map(comment =>
-      new CommentWithUsername({
-        id: comment.id,
-        content: comment.content,
-        createdAt: comment.createdAt,
-        deletedAt: comment.deletedAt,
-        username: comment.commenter.username
-      }).asObject
-    )
   }
 
   async deleteComment(commentId: string): Promise<void> {
