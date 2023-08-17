3a932528f539eb57655a07f065c5f98dc95f8630
ramandabhijana <abhijanaramanda@gmail.com>
2023-08-17 13:23:19 +0800

    add abstract isCommentLikedBy

--- src/Domains/comments/CommentRepositoryBase.ts
+++ src/Domains/comments/CommentRepositoryBase.ts
@@ -8,6 +8,7 @@ abstract class CommentRepositoryBase {
   abstract verifyCommentOwner(commentId: string, owner: string): Promise<void>
   abstract getCommentRepliesByCommentIds(commentIds: string[], repliesQueryOptions?: Partial<PaginationOptions>): Promise<Map<string, CommentWithReplies>>
   abstract deleteComment(commentId: string): Promise<void>
+  abstract isCommentLikedBy(userId: string, commentId: string): Promise<boolean>
   abstract likeComment(commentId: string): Promise<void>
   abstract dislikeComment(commentId: string): Promise<void>
 }
