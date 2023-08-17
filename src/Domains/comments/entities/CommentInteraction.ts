f1ac616a55b8d65604df925b61dbe14ae6f30ad5
ramandabhijana <abhijanaramanda@gmail.com>
2023-08-17 11:43:37 +0800

    fix: rename DeleteComment & DeleteCommentPayload

--- src/Domains/comments/entities/CommentInteraction.ts
+++ src/Domains/comments/entities/CommentInteraction.ts
@@ -1,22 +1,22 @@
 import Joi from 'joi'
 import DomainEntity from '../../DomainEntity'
 
-export interface DeleteCommentPayload {
+export interface CommentInteractionPayload {
   threadId: string
   commentId: string
   userId: string
 }
 
-class DeleteComment extends DomainEntity<DeleteCommentPayload> {
+class CommentInteraction extends DomainEntity<CommentInteractionPayload> {
   get threadId(): string { return this.payload.threadId }
   get commentId(): string { return this.payload.commentId }
   get userId(): string { return this.payload.userId }
 
   protected get entityName(): string {
-    return DeleteComment.name
+    return CommentInteraction.name
   }
 
-  protected get schema(): Joi.PartialSchemaMap<DeleteCommentPayload> {
+  protected get schema(): Joi.PartialSchemaMap<CommentInteractionPayload> {
     return {
       threadId: Joi.string().required(),
       commentId: Joi.string().required(),
@@ -27,4 +27,4 @@ class DeleteComment extends DomainEntity<DeleteCommentPayload> {
   protected shouldTranslateErrorToClientError(): boolean { return true }
 }
 
-export default DeleteComment
+export default CommentInteraction
